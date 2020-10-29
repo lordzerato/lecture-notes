5597f9003f6902e7425faf9036f18b3c4385fda5
lordzerato <fauzidanrama@gmail.com>
2020-10-29 08:40:42 +0700

    Update 1.js

--- rmt-07-glasgow/w1d4s1_nested-loop/soal/1.js
+++ rmt-07-glasgow/w1d4s1_nested-loop/soal/1.js
@@ -2,6 +2,7 @@
 
 /*
     3 elemen yang harus ada di dalam loop?
+    statements, conditions, action/ execution/ proses
 */
 
 
@@ -13,3 +14,18 @@
  *   Input: this is javascript
  *   Output: This Is Javascript
  */
+
+var input = "this is javascript";
+var output = "";
+
+for (var i = 0; i < input.length; i++){
+    if (i === 0) {
+        output = output + input[i].toUpperCase();
+    } else if (i != 0 && input[i-1] === " "){
+        output = output + input[i].toUpperCase();
+    } else {
+        output = output + input[i].toLowerCase();
+    }
+}
+
+console.log(output);
